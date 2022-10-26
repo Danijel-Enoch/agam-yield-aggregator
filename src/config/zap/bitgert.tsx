7751825b5a4756b8a28a4a10eddc68fd9a400a10
Danijel-Enoch <enochdanijel@gmail.com>
2022-10-26 14:44:18 +0100

    Update/added bitgert vault contract

---
+++ src/config/zap/bitgert.tsx
@@ -0,0 +1,10 @@
+export const zaps = [
+	{
+		// BriseSwap
+		zapAddress: '0x697DF4BA702D138ABc23575030ABE54e47c3D227',
+		ammRouter: '0xE396407e21F7d7526ff0b0a8912751C64957fBF7',
+		ammFactory: '0x1379a7f0bfc346d48508B4b162c37a4c43dd89dc',
+		ammPairInitHash: '0xbaa7e495fca8ae538cbbdfd28b1411fff956b63559959ca8d102ebbba2023a13',
+		lpProviderFee: 0.0025,
+	},
+];
